f8f97975f6a0a5dff7248eb961062e0c8e4ea025
PascalRombach <pasc.rombach@heinrichheinedo.de>
2022-08-29 21:32:46 +0200

    Added support for modifiers in dice strings

--- source/_static/common_funcs.js
+++ source/_static/common_funcs.js
@@ -1,22 +1,25 @@
 const roll_dice= (dice) => {
     res= 0;
-    dice.forEach(([n, d]) => {
+    dice[0].forEach(([n, d]) => {
         for (let i = 0; i < n; i++) res+= Math.floor(d*Math.random())+1;
     });
 
-    return res;
+    return res + dice[1];
 };
 
 const decode_die_string= string => {
     let res= [];
+    let modifier= 0;
     
     string.split("+").forEach(die=>{
-        let [n, d]= die.split("d",2);
+        if (die.indexOf("d") != -1){
+            let [n, d]= die.split("d",2);
 
-        res.push([Number.parseInt(n),Number.parseInt(d)]);
+            res.push([Number.parseInt(n),Number.parseInt(d)]);
+        } else modifier+= Number.parseInt(die);
     });
 
-    return res;
+    return [res, modifier];
 };
 
 const rpad= (string,padding_length,padding_char=" ") => {
